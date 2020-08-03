04ea1101c578d2ea04318db3e289a734a1e4163a
unknown <khanvuthy88@gmail.com>
2020-08-03 12:26:44 +0700

    Update slide

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -13,7 +13,7 @@ class Website(Website):
     def index(self, data={}, **kw):
         super(Website, self).index(**kw)
         base_url = 'http://sithikarealty.com'
-        show_slide = true
+        show_slide = True
         has_property = False
         property_obj = request.env['khmerrealty.property']
         location_obj = request.env['khmerrealty.property.location'].search([('parent_id', '=', False)])
